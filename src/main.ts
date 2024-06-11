7513f617e00783d09c9691296b8a7e3f3dca0c72
Matěj Pešl <matejpesl1@gmail.com>
2024-06-11 03:17:56 +0200

    feat: implement fastify instead of express

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,21 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import "dotenv/config";
 import { ValidationPipe } from "@nestjs/common";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(process.env.PORT ?? 3000, "0.0.0.0");
 }
 
 bootstrap();
