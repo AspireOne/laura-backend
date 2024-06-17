fed9c88908f7467c1a299d94a0b7cc1c0a0b3c6e
Matěj Pešl <matejpesl1@gmail.com>
2024-06-18 01:26:08 +0200

    Added GoogleOauthTokenManagementService to BirthdayReminderService dependencies.

--- src/schedulers/birthday-reminder.service.spec.ts
+++ src/schedulers/birthday-reminder.service.spec.ts
@@ -1,16 +1,17 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { BirthdayReminderService } from './birthday-reminder.service';
-import { SchedulerErrorHandlerService } from '../common/services/scheduler-error-handler.service';
-import { GoogleOauthClientService } from '../common/services/google-oauth-client.service';
-import { ContactsService } from '../common/services/contacts.service';
-import { ProvidersModule } from '../common/providers/providers.module';
-import { DatabaseProvider } from '../common/providers/database.provider';
-import { ExpoProvider } from '../common/providers/expo.provider';
-import { FirebaseAdminProvider } from '../common/providers/firebase-admin.provider';
-import { OpenAIProvider } from '../common/providers/openai.provider';
-import { GoogleOAuthProvider } from '../common/providers/google-oauth.provider';
+import { Test, TestingModule } from "@nestjs/testing";
+import { BirthdayReminderService } from "./birthday-reminder.service";
+import { SchedulerErrorHandlerService } from "../common/services/scheduler-error-handler.service";
+import { GoogleOauthClientService } from "../common/services/google-oauth-client.service";
+import { ContactsService } from "../common/services/contacts.service";
+import { ProvidersModule } from "../common/providers/providers.module";
+import { DatabaseProvider } from "../common/providers/database.provider";
+import { ExpoProvider } from "../common/providers/expo.provider";
+import { FirebaseAdminProvider } from "../common/providers/firebase-admin.provider";
+import { OpenAIProvider } from "../common/providers/openai.provider";
+import { GoogleOAuthProvider } from "../common/providers/google-oauth.provider";
+import { GoogleOauthTokenManagementService } from "src/common/services/google-oauth-token-management.service";
 
-describe('BirthdayReminderService', () => {
+describe("BirthdayReminderService", () => {
   let service: BirthdayReminderService;
 
   beforeEach(async () => {
@@ -19,6 +20,7 @@ describe('BirthdayReminderService', () => {
         BirthdayReminderService,
         SchedulerErrorHandlerService,
         GoogleOauthClientService,
+        GoogleOauthTokenManagementService,
         ContactsService,
         DatabaseProvider,
         ExpoProvider,
@@ -31,11 +33,11 @@ describe('BirthdayReminderService', () => {
     service = module.get<BirthdayReminderService>(BirthdayReminderService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 
-  it('handleCron should not throw an error', async () => {
+  it("handleCron should not throw an error", async () => {
     await expect(service.handleCron()).resolves.not.toThrow();
   });
 });
