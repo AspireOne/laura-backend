66f125986024b350a960753589cbb70ed5cbaa69
Matěj Pešl <matejpesl1@gmail.com>
2024-08-23 00:09:34 +0200

    refactor(schedulers): remove unused DailyQuote and DailyFact scheduler services

--- src/schedulers/birthday/birthday-reminder.module.ts
+++ src/schedulers/birthday/birthday-reminder.module.ts
@@ -1,8 +1,4 @@
 import { Module } from "@nestjs/common";
-import { ScheduleModule } from "@nestjs/schedule";
-import { ProvidersModule } from "src/common/providers/providers.module";
-import { ServicesModule } from "src/common/services/services.module";
-import { DailyQuoteSchedulerService } from "src/schedulers/daily-quote.scheduler.service";
 import { DbPruneService } from "src/schedulers/db-prune.scheduler.service";
 import { TestSchedulerService } from "src/schedulers/test.scheduler.service";
 import {
@@ -12,7 +8,6 @@ import {
 @Module({
   imports: [],
   providers: [
-    DailyQuoteSchedulerService,
     DbPruneService,
     TestSchedulerService,
     BirthdayReminderSchedulerService,
--- src/schedulers/schedulers.module.ts
+++ src/schedulers/schedulers.module.ts
@@ -1,4 +1,3 @@
-import { DailyQuoteSchedulerService } from "./daily-quote.scheduler.service";
 import { Module } from "@nestjs/common";
 import { ScheduleModule } from "@nestjs/schedule";
 import { TestSchedulerService } from "./test.scheduler.service";
@@ -6,16 +5,13 @@ import { ProvidersModule } from "../common/providers/providers.module";
 import { ServicesModule } from "../common/services/services.module";
 import { DbPruneService } from "./db-prune.scheduler.service";
 import { BirthdayReminderSchedulerService } from "src/schedulers/birthday/birthday-reminder.scheduler.service";
-import { DailyFactSchedulerService } from "src/schedulers/daily-fact.scheduler.service";
 
 @Module({
   imports: [ScheduleModule.forRoot(), ProvidersModule, ServicesModule],
   providers: [
-    DailyQuoteSchedulerService,
     DbPruneService,
     TestSchedulerService,
     BirthdayReminderSchedulerService,
-    DailyFactSchedulerService,
   ],
 })
 export class SchedulersModule {}
